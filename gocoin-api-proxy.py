b2d5bc4f1f4d8b946f34d4baf73bbcd54325e245
CCI-Richard <richard@gocoin.org>
2012-02-07 02:22:07 +1000

    Added first copy of the gocoin-api-proxy script for Google App Engine

---
+++ gocoin-api-proxy.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+import logging
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp import util
+from google.appengine.api import urlfetch
+
+class MainHandler(webapp.RequestHandler):
+    def get(self):
+    	self.response.headers['Access-Control-Allow-Origin'] = 'http://gocoin.org'
+        headers = {}
+    	for x in self.request.headers:
+        	if (x != "X-Apiurl" and x[0:2] != "Ac"):
+        		headers[x] = self.request.headers[x]
+       	r = urlfetch.fetch(self.request.headers["X-Apiurl"], None, "GET", headers, True, True, None, None)
+        if (r.status_code != 200):
+        	self.response.set_status(r.status_code)
+        	self.response.out.write("Bad request: " + str(r.status_code))
+        else:
+        	self.response.out.write(r.content)
+        
+    def post(self):
+    	self.response.headers['Access-Control-Allow-Origin'] = 'http://gocoin.org'
+        headers = {}
+        for x in self.request.headers:
+        	if (x != "X-Apiurl" and x[0:2] != "Ac"):
+        		headers[x] = self.request.headers[x]
+       	r = urlfetch.fetch(self.request.headers['X-Apiurl'], self.request.body, "POST", headers, True, True, None, None)
+        if (r.status_code != 200):
+        	self.response.set_status(r.status_code)
+        	self.response.out.write("Bad request: " + str(r.status_code))
+        else:
+        	if ('location' in r.headers):
+        		self.response.out.write(r.headers['location'])
+        	else:
+				self.response.out.write(r.content)
+        	
+    def put(self):
+    	self.response.headers['Access-Control-Allow-Origin'] = 'http://gocoin.org'
+        headers = {}
+        for x in self.request.headers:
+        	if (x != "X-Apiurl" and x[0:2] != "Ac" and x[0:2] == "Re"):
+        		headers[x] = self.request.headers[x]
+        for x in headers:
+        	logging.info("-H \"" + x + ": " + headers[x] + "\" ")
+        logging.info("\"" + self.request.headers['X-Apiurl'] + "\"") 
+       	r = urlfetch.fetch(self.request.headers['X-Apiurl'], self.request.body, "PUT", headers, True, True, None, None)
+        if (r.status_code != 200):
+        	self.response.set_status(r.status_code)
+        	self.response.out.write("" + str(r.status_code))
+        else:
+        	if ('location' in r.headers):
+        		self.response.out.write(r.headers['location'])
+        	else:
+				self.response.out.write(r.content)
+
+				    	
+    def options(self):
+    	self.response.headers['Access-Control-Allow-Origin'] = 'http://gocoin.org'
+      	self.response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS'
+    	if ('Access-Control-Request-Headers' in self.request.headers):
+    		self.response.headers['Access-Control-Allow-Headers'] = self.request.headers['Access-Control-Request-Headers']
+    	
+    		
+def main():
+    application = webapp.WSGIApplication([('/', MainHandler)],
+                                         debug=True)
+    util.run_wsgi_app(application)
+
+
+if __name__ == '__main__':
+    main()
